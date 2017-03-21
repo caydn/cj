61ce76a4909d1312a42aa26b76b22a0a57766590
Cayden Jacobs <cayden.jacobs@yahoo.com>
2017-03-21 01:41:10 -0400

    added friendly_id slug gem

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -68,11 +68,11 @@ class ProjectsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+      @project = Project.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :image, :description, :summary)
+      params.require(:project).permit(:title, :image, :description, :summary, :slug)
     end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,11 @@
 class Project < ApplicationRecord
   validates_presence_of :title, :image, :description, :summary
 
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+  def should_generate_new_friendly_id?
+   slug.blank? || title_changed?
+ end
+
   mount_uploader :image, PhotoUploader
 end
