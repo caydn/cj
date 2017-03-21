5457038b6d536e35e1535f87b7142e9685fca98a
Cayden Jacobs <cayden.jacobs@yahoo.com>
2017-03-21 16:58:41 -0400

    reverted fob initializer to previous version

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -14,15 +14,6 @@ if Rails.env.production?
     config.fog_directory  = ENV['S3_BUCKET']
     # config.fog_use_ssl_for_aws = false
     # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-  else
-    config.fog_credentials = {
-      provider:               'AWS',
-      aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                 ENV['AWS_REGION']
-
-    }
-    config.fog_directory  = ENV['S3_BUCKET']
     config.fog_public     = false
 
   end
