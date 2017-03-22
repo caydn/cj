9a419977bb4acafdcbf9fbb16675dbde65d6fab0
Cayden Jacobs <cayden.jacobs@yahoo.com>
2017-03-22 11:45:57 -0400

    styled devise pg layout, edited site styling overall

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :projects
   resources :contacts, only: [:new, :create]
-
   get 'welcome/index'
   root 'welcome#index'
+
+  get '*path' => redirect('/')
 end
